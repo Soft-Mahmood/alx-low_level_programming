22ab18ce51dcac9990182ec333eeccb0ecd4148a
Soft-Mahmood <softmahmood@gmail.com>
2023-05-31 23:05:22 +0300

    tasks 3 and 4

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * this function returns the length of a string
+ * s is the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == "\0")
+	{
+		return;
+
+	return (1 + _strlen_recursion(s + 1)
+
+	}
+}
