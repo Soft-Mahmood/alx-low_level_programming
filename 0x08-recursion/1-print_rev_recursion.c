293dd1c7af84972bd9a1ac9b5bc155c7b69da658
Soft-Mahmood <softmahmood@gmail.com>
2023-05-31 21:38:12 +0300

    task 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 1-print_rev_recursion - prints a string s in reverse,
+ * followed by a new line
+ */
+void _print_rev_recursion(char *s)
+{
+	int n
+	n=[s]
+
+	if (n = 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(n-1);
+	_puts_recursion(n-1);
+}
