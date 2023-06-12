3e008849887aa78962b3786edc9b238e84518809
Soft-Mahmood <softmahmood@gmail.com>
2023-06-12 17:29:18 +0300

    used the right library

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#include <stdlib>
-/*
- * this program prints 
- * of the file it was compiled from 
- */
+#include <stdio.h>
 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{ 
-	
+{
 	printf("%s\n", __FILE__);
 	return (0);
 }
