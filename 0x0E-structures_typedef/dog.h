c3114b2511bf144d721ea587cca11b7fd6196b5e
Soft-Mahmood <softmahmood@gmail.com>
2023-06-19 23:56:25 +0300

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * this file defines a new struct
+ * dog. with elements
+ * name, age, and owner
+ *
+ **/ 
+
+struct dog {
+
+	char *name;
+	float age;
+	char *owner;
+};
