993af7c4c555e26b291ba4c35306644b72f79d6f
Soft-Mahmood <softmahmood@gmail.com>
2023-07-26 23:26:55 -0700

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * binary_to_unit converts a binary number to 
+ * binary_to_unit - converts a binary number to
  * unsigned int.
- * @b points to a string of 0s & 1s
- * returns the converted number or 
- * returns 0 if one or more charsacters in the string  is not a 0 or a 1
+ * @b: points to a string of 0s & 1s
+ * Return: the converted number or
+ * 0 if one or more charsacters in the string  is not a 0 or a 1
  * also returns 0 is b is null
  */
 
