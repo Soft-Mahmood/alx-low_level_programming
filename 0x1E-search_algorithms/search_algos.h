8bcffe3c389698a6d94df397f390c4109657cb95
Soft Mahmood <softmahmood@gmail.com>
2024-01-16 12:26:18 -0800

    jumping

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_index(int *array, size_t l, size_t r, int value);
 void print_array(int *array, size_t l, size_t r);
+int jump_search(int *array, size_t size, int value);
+size_t minimum_value(size_t a, size_t b);
 
 #endif
